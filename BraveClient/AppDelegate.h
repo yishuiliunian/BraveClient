c2c93f8147497d8d590efead8c205f2b43bcfb03
dongzhao <yishuiliunian@gmail.com>
2015-06-17 09:58:36 +0800

    Initial Commit

---
+++ BraveClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BraveClient
+//
+//  Created by baidu on 15/6/17.
+//  Copyright (c) 2015年 dzpqzb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BraveClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BraveClient
+//
+//  Created by baidu on 15/6/17.
+//  Copyright (c) 2015年 dzpqzb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
